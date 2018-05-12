27bf5071e0d4fe75261e90ad665dc516b130e215
atheodosiou <anastasios.theodosiou@gmail.com>
2018-05-12 23:14:11 +0300

    Add new class to work in parallel

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -27,9 +27,11 @@ object Main {
 
     val graph = importDataset.ImportGraph(sc,DATASET_PATH)
 
+
     val allEdges = graph.edges.map(item => (item.srcId,item.dstId))
     val neighbors = graph.collectNeighbors(EdgeDirection.Either).groupByKey()
       .mapValues(l3 => l3.flatMap(l4=>l4.map(l5 => l5._1):Seq[Long]).toSeq: Seq[Long])
+
 
     neighbors.foreach(println)
 
@@ -50,21 +52,13 @@ object Main {
     println()
 
 
-    // TASOS
 
 
-    val edgeDF = ss.sql("Select _1, _2 from allEdges")
-      .withColumnRenamed("_1","ae1")
-      .withColumnRenamed("_2","ae2").show()
 
-    val neighborsDF = ss.sql("Select _1, _2 from neighbors")
-      .withColumnRenamed("_1","n1")
-      .withColumnRenamed("_2","n2").show()
-
-
-   // val joindDF=edgeDF.
-
-
+    
+    //================ TASOS ======================================
+    val ts=new testing()
+    ts.TriangleCount(graph,ss)
   }
 }
 
