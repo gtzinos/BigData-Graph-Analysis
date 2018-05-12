8e7970e92d16b5294fe00c08e08f46bf22b428a7
atheodosiou <anastasios.theodosiou@gmail.com>
2018-05-12 22:53:32 +0300

    sql changes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -28,7 +28,8 @@ object Main {
     val graph = importDataset.ImportGraph(sc,DATASET_PATH)
 
     val allEdges = graph.edges.map(item => (item.srcId,item.dstId))
-    val neighbors = graph.collectNeighbors(EdgeDirection.Either).groupByKey().mapValues(l3 => l3.flatMap(l4=>l4.map(l5 => l5._1):Seq[Long]).toSeq: Seq[Long])
+    val neighbors = graph.collectNeighbors(EdgeDirection.Either).groupByKey()
+      .mapValues(l3 => l3.flatMap(l4=>l4.map(l5 => l5._1):Seq[Long]).toSeq: Seq[Long])
 
     neighbors.foreach(println)
 
@@ -45,6 +46,25 @@ object Main {
     ss.sql("Select * from allEdges ae, neighbors ne " +
       "where ae._1 = ne._1 or ae._2 = ne._1" +
       "").show()
+
+    println()
+
+
+    // TASOS
+
+
+    val edgeDF = ss.sql("Select _1, _2 from allEdges")
+      .withColumnRenamed("_1","ae1")
+      .withColumnRenamed("_2","ae2").show()
+
+    val neighborsDF = ss.sql("Select _1, _2 from neighbors")
+      .withColumnRenamed("_1","n1")
+      .withColumnRenamed("_2","n2").show()
+
+
+   // val joindDF=edgeDF.
+
+
   }
 }
 
